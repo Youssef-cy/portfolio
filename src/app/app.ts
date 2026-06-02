57e76747ba64075737582d57a273f8d2e3b2aeab
Youssef-cy <alpha112534@gmail.com>
2026-06-02 18:57:34 +0300

    add skill section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from "./layout/nav-bar/nav-bar";
 import { HeroSection } from "./feature/hero-section/hero-section";
+import { AboutMe } from "./feature/about-me/about-me";
+import { Skill } from "./feature/skill/skill";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar, HeroSection],
+  imports: [RouterOutlet, NavBar, HeroSection, AboutMe, Skill],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
